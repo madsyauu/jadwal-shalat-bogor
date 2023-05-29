7a169ff9be7bab8ad5ff435023ce044f8377f902
Alfa Nugraha <alfa.nugraha4@gmail.com>
2023-05-29 17:02:19 +0700

    Add scrape and yml action

---
+++ scrape.R
@@ -0,0 +1,42 @@
+# Set library ----
+message("Load the libraries")
+library(rvest)
+library(mongolite)
+
+# List of covid sites
+# https://news.google.com/covid19/map
+# https://www.worldometers.info/coronavirus/country/indonesia/
+# https://corona.jakarta.go.id/id/data-pemantauan
+# https://kawalcovid19.id/
+# https://covid19.go.id/peta-sebaran
+
+message("Define function to scrape OP character")
+url <- "https://www.worldometers.info/coronavirus/country/indonesia/"
+html <- read_html(url)
+count <- html_text(html_nodes(html, ".maincounter-number"), trim=T)
+
+# collection <- "cities"
+# db <- "prak12"
+# url <- "mongodb://localhost:27017/"
+# cities <- mongo(
+#   collection=collection, 
+#   db=db, 
+#   url=url
+# )
+
+message("Connect to MongoDB Cloud")
+atlas <- mongo(
+  collection = Sys.getenv("ATLAS_COLLECTION"),
+  db         = Sys.getenv("ATLAS_DB"),
+  url        = Sys.getenv("ATLAS_URL")
+)
+
+# covid <- data.frame(no=integer(), cases=character(), deaths=character(), recovered=character())
+newcovid <- data.frame(no = atlas$count() + 1, cases = count[1], deaths = count[2], recovered = count[3])
+
+message("Store data frame into mongo cloud")
+if(atlas$count() > 0)
+  atlas$drop()
+atlas$insert(newcovid)
+
+atlas$disconnect()
